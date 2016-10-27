22bc514fbd3b8a65a5f778e203402efc1f6f5c81
Joe <joe.biggica@gmail.com>
2016-10-27 16:56:52 -0400

    updates

--- client/helpers/user-helpers.js
+++ client/helpers/user-helpers.js
@@ -5,11 +5,14 @@ Template.logout.helpers({
 	username: function() {
 		return Meteor.user().username;
 	}
-})
+});
 
 Template.userList.helpers({
 	firstName: function() {
 		return Meteor.users.findOne(this._id).profile.firstname;
+	},
+	lastName: function() {
+		return Meteor.users.findOne(this._id).profile.lastname;
 	},
 	email: function() {
 		return Meteor.users.findOne(this._id).emails[0].address;
@@ -31,6 +34,9 @@ Template.userPage.helpers({
 	email: function() {
 		var email = Meteor.users.findOne(this.userObject._id).emails[0].address;
 		return email;
+	},
+	username: function() {
+		return Meteor.users.findOne(this.userObject._id).username
 	},
 	userObject: function() {
 		var user = Meteor.users.findOne(this.userObject._id);
