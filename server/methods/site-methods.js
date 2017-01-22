68b21d975808f81843626aab23e3ea30615527cf
JoeBiggia <jbaki91@yahoo.com>
2017-01-22 14:33:22 -0500

    added sitemap

--- server/methods/site-methods.js
+++ server/methods/site-methods.js
@@ -3,15 +3,14 @@ Meteor.methods({
 	ssrSitemap: function(){
 		SSR.compileTemplate('sitemap', Assets.getText('sitemap.txt'));
 		var articles = Articles.find({}).fetch();
-
+		var urlList = '';
 		for(i=0; articles.length > i; i++){
-			console.log(articles[i])
+			urlList = urlList + 'http://www.comicadvisors.com/article/'+articles[i].slug+'/'+articles[i]._id+'\r\n'
 		}
-
-
-
-
-	    var html = SSR.render('sitemap');
+		
+	    var html = SSR.render('sitemap',{
+	    	urlList: urlList
+	    });
 	    return html;
 	}
 });
