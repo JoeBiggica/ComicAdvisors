47ffa5ed04ba4a835d7c65edbec9e58907861b84
JoeBiggia <jbaki91@yahoo.com>
2017-05-08 23:11:18 -0400

    pagination work

--- client/helpers/section-helpers.js
+++ client/helpers/section-helpers.js
@@ -2,4 +2,18 @@ Template.sectionArticlesPage.helpers({
 	section: function() {
 		return Session.get("querySection");
 	}
+});
+
+Template.paginationControls.helpers({
+	controls: function() {
+		if (this.page_number) {
+			const next_page = this.page_number + 1;
+			const control_url = '/articles/' + next_page.toString() + '#';
+			let control_button = '<a href="' + control_url + '" ><div class="pagination-button dark">Previous Articles <span class="pagination-arrow">&#10095</span></div></a>'
+			return control_button;
+		} else {
+			let control_button = '<a href="/articles/1#" ><div class="pagination-button dark">Previous Articles <span class="pagination-arrow">&#10095</span></div></a>'
+			return control_button;
+		}
+	}
 })
