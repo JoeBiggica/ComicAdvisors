7fa5423943250ada81fc4a4843b328fe0988fb5c
JoeBiggia <jbaki91@yahoo.com>
2017-05-08 23:24:25 -0400

    pagination working

--- client/helpers/section-helpers.js
+++ client/helpers/section-helpers.js
@@ -8,11 +8,11 @@ Template.paginationControls.helpers({
 	controls: function() {
 		if (this.page_number) {
 			const next_page = this.page_number + 1;
-			const control_url = '/articles/' + next_page.toString() + '#';
+			const control_url = '/articles/' + next_page.toString();
 			let control_button = '<a href="' + control_url + '" ><div class="pagination-button dark">Previous Articles <span class="pagination-arrow">&#10095</span></div></a>'
 			return control_button;
 		} else {
-			let control_button = '<a href="/articles/1#" ><div class="pagination-button dark">Previous Articles <span class="pagination-arrow">&#10095</span></div></a>'
+			let control_button = '<a href="/articles/1" ><div class="pagination-button dark">Previous Articles <span class="pagination-arrow">&#10095</span></div></a>'
 			return control_button;
 		}
 	}
