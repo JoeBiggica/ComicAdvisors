118fa837cdce51730265e074c6c39f9c6d6f2343
JoeBiggia <jbaki91@yahoo.com>
2017-06-20 22:31:48 -0400

    fixed article query issue

--- server/publications/article-publications.js
+++ server/publications/article-publications.js
@@ -6,6 +6,10 @@ Meteor.publish('singleArticle', function(id) {
 	return Articles.find(id);
 });
 
+Meteor.publish('selectArticles', function(index) {
+	return Articles.find({}, { sort: { createdAt: -1 }, limit: 10, skip: index  });
+});
+
 Meteor.publish('sectionArticles', function(section) {
 	return Articles.find({section: section});
 });
